6d7cc9d55364a2619a039802a6d9fe3a523add7f
nraphael <nraphael@miunbound.com>
2022-06-30 00:03:10 -0400

    Added a UI for Job entry that links to the main menu and returns to the main menu. Other buttons are initialized, but not programmed yet. Builds correctly.

---
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Job_Entry.java
@@ -0,0 +1,38 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Job_Entry extends AppCompatActivity {
+
+    private Button returnToMainMenuBtn;
+    private Button saveBtn;
+    private Button cancelBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job__entry);
+
+        returnToMainMenuBtn = (Button)findViewById(R.id.mainMenuBtn);
+        returnToMainMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainMenu();
+            }
+        });
+    }
+
+    public void openMainMenu() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+}
