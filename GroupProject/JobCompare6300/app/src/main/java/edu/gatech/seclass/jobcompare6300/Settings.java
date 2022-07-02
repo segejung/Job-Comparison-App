7548c95f98e79709bfc6c16b6f43259e78729663
nraphael <nraphael@miunbound.com>
2022-07-02 12:13:05 -0400

    Added toast button notifications for settings.

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Settings.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Settings.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -24,10 +25,26 @@ public class Settings extends AppCompatActivity {
                 returnToMainMenu();
             }
         });
+
+        saveSettingsBtn = (Button)findViewById(R.id.saveSettingsButton);
+        saveSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSettingsForUser();
+            }
+        });
     }
 
     public void returnToMainMenu() {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+        Toast.makeText(Settings.this,"Returning to main menu",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveSettingsForUser() {
+        Intent intent = new Intent(this,Settings.class);
+        Toast.makeText(Settings.this,
+                "Job Weight Settings Save Action (TBC)",Toast.LENGTH_SHORT).show();
     }
 }
