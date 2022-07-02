b6b54466d4c13384d32322d8bdb4127de4791254
nraphael <nraphael@miunbound.com>
2022-07-02 10:50:33 -0400

    Changed the Recycler to the List View and persistent values display in the app after shutdown.

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/All_Jobs_Display.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/All_Jobs_Display.java
@@ -1,12 +1,16 @@
 package edu.gatech.seclass.jobcompare6300;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -16,11 +20,14 @@ public class All_Jobs_Display extends AppCompatActivity {
     private Button viewAllBtn;
     private Button compareSelectedBtn;
     private Button cancelCompareBtn;
+    private ListView jobListDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all__jobs__display);
+
+        jobListDisplay = findViewById(R.id.jobListEntries);
 
         viewAllBtn = (Button)findViewById(R.id.viewJobsBtn);
         viewAllBtn.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +35,13 @@ public class All_Jobs_Display extends AppCompatActivity {
             public void onClick(View v) {
                 DataBaseHelper databaseHelper = new DataBaseHelper(All_Jobs_Display.this);
                 List<JobDetails> jobListEntries = databaseHelper.getOffers();
+
+                ArrayAdapter jobOfferListArrayAdapter = new ArrayAdapter<JobDetails>(All_Jobs_Display.this,
+                        android.R.layout.simple_list_item_1,jobListEntries);
+                jobListDisplay.setAdapter(jobOfferListArrayAdapter);
+
+                //Toast.makeText(All_Jobs_Display.this,jobListEntries.toString(),Toast.LENGTH_SHORT).show();
+
                 Toast.makeText(All_Jobs_Display.this,"Jobs from DB are displayed",
                         Toast.LENGTH_SHORT).show();
 
