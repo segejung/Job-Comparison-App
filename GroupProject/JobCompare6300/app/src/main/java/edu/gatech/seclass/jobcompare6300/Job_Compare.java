628c79a81a9885dae501f66a4007d71d80b93aa8
nraphael <nraphael@miunbound.com>
2022-07-06 19:28:43 -0400

    Fixed app crash issue with new job compare layout and merged the updated layout with the old linked file on the manifest

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Job_Compare.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Job_Compare.java
@@ -7,18 +7,28 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Job_Compare extends AppCompatActivity {
 
     private Button goBackBtn;
+    private Button compareAgainBtn;
     public SQLiteDatabase appDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job__compare_new);
+        setContentView(R.layout.activity_job__compare);
 
-        goBackBtn = (Button)findViewById(R.id.goBackButton);
+        compareAgainBtn = (Button)findViewById(R.id.compareAgainButton);
+        compareAgainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compareJobsAgain();
+            }
+        });
+
+        goBackBtn = (Button)findViewById(R.id.returnToMainButton);
         goBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,5 +40,17 @@ public class Job_Compare extends AppCompatActivity {
     public void openMainMenu() {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+        Toast.makeText(Job_Compare.this,"Returning to Main Menu",
+                Toast.LENGTH_SHORT).show();
     }
+
+    public void compareJobsAgain() {
+        Intent intent = new Intent(this,All_Jobs_Display.class);
+        startActivity(intent);
+        Toast.makeText(Job_Compare.this,"Redoing Comparison",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
