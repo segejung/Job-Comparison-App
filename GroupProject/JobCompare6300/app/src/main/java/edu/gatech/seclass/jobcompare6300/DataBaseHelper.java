c9066d14c17f258a13ac16100fbd02e65bf45a4c
nraphael <nraphael@miunbound.com>
2022-07-10 19:20:41 -0400

    Cleaned up code.

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/DataBaseHelper.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/DataBaseHelper.java
@@ -40,13 +40,13 @@ public class DataBaseHelper extends  SQLiteOpenHelper{
     public static final String COLUMN_RBP = "COLUMN_RBP";
     public static final String COLUMN_RS = "COLUMN_RS";
     public static final String COLUMN_TDF = "COLUMN_TDF";
-    public static final String COLUMN_AYS_VAL = "COLUMN_AYS_VAL";
-    public static final String COLUMN_AYB_VAL = "COLUMN_AYB_VAL";
-    public static final String COLUMN_RB_VAL = "COLUMN_RB_VAL";
-    public static final String COLUMN_RA_VAL = "COLUMN_RA_VAL";
-    public static final String COLUMN_TDF_VAL = "COLUMN_TDF_VAL";
+//    public static final String COLUMN_AYS_VAL = "COLUMN_AYS_VAL";
+//    public static final String COLUMN_AYB_VAL = "COLUMN_AYB_VAL";
+//    public static final String COLUMN_RB_VAL = "COLUMN_RB_VAL";
+//    public static final String COLUMN_RA_VAL = "COLUMN_RA_VAL";
+//    public static final String COLUMN_TDF_VAL = "COLUMN_TDF_VAL";
     public static final String JOB_ID = "JOB_ID";
-    public static final String JOB_CALC_WEIGHTS_TABLE = "JOB_CALC_WEIGHTS_TABLE";
+//    public static final String JOB_CALC_WEIGHTS_TABLE = "JOB_CALC_WEIGHTS_TABLE";
     public static final Integer dbVersion = 2;
     public static final String COLUMN_STATUS = "COLUMN_STATUS";
 
@@ -74,14 +74,14 @@ public class DataBaseHelper extends  SQLiteOpenHelper{
 
 
 
-        String createRankTableStatement =
-                "CREATE TABLE " + JOB_CALC_WEIGHTS_TABLE + " (" + JOB_ID + " INTEGER PRIMARY KEY, " + COLUMN_AYS_VAL +
-                        " INTEGER, " + COLUMN_AYB_VAL + " INTEGER, " + COLUMN_RB_VAL +
-                        " INTEGER, " + COLUMN_RA_VAL + " INTEGER, " + COLUMN_TDF_VAL + " INTEGER)";
+//        String createRankTableStatement =
+//                "CREATE TABLE " + JOB_CALC_WEIGHTS_TABLE + " (" + JOB_ID + " INTEGER PRIMARY KEY, " + COLUMN_AYS_VAL +
+//                        " INTEGER, " + COLUMN_AYB_VAL + " INTEGER, " + COLUMN_RB_VAL +
+//                        " INTEGER, " + COLUMN_RA_VAL + " INTEGER, " + COLUMN_TDF_VAL + " INTEGER)";
 //
         db.execSQL(createDBTableStatement);
         db.execSQL(createDBWeightTableStatement);
-        db.execSQL(createRankTableStatement);
+//        db.execSQL(createRankTableStatement);
 
         this.setWeightsDefault(db);
 //
@@ -92,10 +92,10 @@ public class DataBaseHelper extends  SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String queryStrMainTable = "DROP TABLE IF EXISTS " + JOB_OFFER_TABLE;
         String queryStrWeightTable = "DROP TABLE IF EXISTS " + JOB_WEIGHT_TABLE;
-        String queryStrRankTable = "DROP TABLE IF EXISTS " + JOB_CALC_WEIGHTS_TABLE;
+//        String queryStrRankTable = "DROP TABLE IF EXISTS " + JOB_CALC_WEIGHTS_TABLE;
         db.execSQL(queryStrMainTable);
         db.execSQL(queryStrWeightTable);
-        db.execSQL(queryStrRankTable);
+//        db.execSQL(queryStrRankTable);
         onCreate(db);
 
     }
@@ -631,94 +631,8 @@ public class DataBaseHelper extends  SQLiteOpenHelper{
         return returnedJobOffers;
     }
 
-    public List<JobRankDetails> getJobDetails() {
-        List<JobRankDetails> returnedJobObjects = new ArrayList<>();
-
-        String queryRequestStr = "SELECT * FROM " + JOB_OFFER_TABLE;
-        SQLiteDatabase appDB = this.getReadableDatabase(); // We just want to read the database here.
-
-        Cursor cursor = appDB.rawQuery(queryRequestStr,null);
 
 
-        if(cursor.moveToFirst()) {
-            // We want to iterate through the list here and create a JobDetails obj for each row
-            do {
-                Integer jobID = cursor.getInt(0);
-                String jobTitle = cursor.getString(1);
-                String jobCompanyName = cursor.getString(2);
-                String jobLocation = cursor.getString(3);
-                Integer jobCostOfLiving = cursor.getInt(4);
-                Integer jobAnnualSalary = cursor.getInt(5);
-                Integer jobAnnualBonus = cursor.getInt(6);
-                Integer jobRetirementBenefits = cursor.getInt(7);
-                Integer jobRelocationStipend = cursor.getInt(8);
-                Integer jobTrainingAndDevFund = cursor.getInt(9);
-                boolean currentJobIndicator = cursor.getInt(10) == 1 ? true: false;
-
-                // Omit job score (this should be hidden)
-
-                JobRankDetails jobObj = new JobRankDetails(jobTitle,jobCompanyName,jobLocation,
-                        jobCostOfLiving,jobAnnualSalary,jobAnnualBonus,jobRetirementBenefits,
-                        jobRelocationStipend,jobTrainingAndDevFund,currentJobIndicator);
-
-                returnedJobObjects.add(jobObj);
-
-            } while (cursor.moveToNext());
-        } else {
-
-            // Empty list
-        }
-
-        //close db connection
-        cursor.close();
-        appDB.close();
-
-        return returnedJobObjects;
-    }
-
-    public List<Pair> getNonCurrentOffersWithIDs() {
-        List<Pair> returnedJobOffersAndIDs = new ArrayList<>();
-
-        // This code will pull data from the database
-
-        String queryRequestStr = "SELECT * FROM " + JOB_OFFER_TABLE + " WHERE " + COLUMN_IS_CURRENT_JOB + " =?";
-        String[] args = {"0"};
-        SQLiteDatabase appDB = this.getReadableDatabase(); // We just want to read the database here.
-
-        Cursor cursor = appDB.rawQuery(queryRequestStr,null);
-
-        if(cursor.moveToFirst()) {
-            // We want to iterate through the list here and create a JobDetails obj for each row
-            do {
-                Integer jobID = cursor.getInt(0);
-                String jobTitle = cursor.getString(1);
-//                String jobCompanyName = cursor.getString(2);
-//                String jobLocation = cursor.getString(3);
-//                Integer jobCostOfLiving = cursor.getInt(4);
-//                Integer jobAnnualSalary = cursor.getInt(5);
-//                Integer jobAnnualBonus = cursor.getInt(6);
-//                Integer jobRetirementBenefits = cursor.getInt(7);
-//                Integer jobRelocationStipend = cursor.getInt(8);
-//                Integer jobTrainingAndDevFund = cursor.getInt(9);
-//                boolean currentJobIndicator = cursor.getInt(10) == 1 ? true: false;
-
-                // Omit job score (this should be hidden)
-
-                Pair<Integer,String> jobPair = new Pair<Integer, String>(jobID,jobTitle);
-                returnedJobOffersAndIDs.add(jobPair);
-
-            } while (cursor.moveToNext());
-        } else {
-
-            // Empty list
-        }
-
-        //close db connection
-        cursor.close();
-        appDB.close();
-
-        return returnedJobOffersAndIDs;
-    }
 
     public void setNewCurrentJob(int jobId) {
 
